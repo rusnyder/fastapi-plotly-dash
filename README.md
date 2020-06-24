c6ac56c9acce557b87d410476755fe60206f82c9
Russell Snyder <russ@arceo.ai>
2020-06-24 14:24:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# FastAPI + Dash Sample App
+Sample application that runs a Dash app mounted inside a FastAPI webserver.
+
+## Quickstart
+
+1. Install dependencies
+   ```bash
+   pipenv sync -d
+   ```
+2. Run webserver
+   ```bash
+   pipenv run server
+   ```
+3. Load up in browser: http://localhost:8000
+
+## Routes
+
+| Route       | Description                      | Link                         |
+|-------------|----------------------------------|------------------------------|
+| GET /       | Base route (tests shared prefix) | http://localhost:8000/       |
+| GET /status | Non-Dash route                   | http://localhost:8000/status |
+| GET /dash   | Dash app                         | http://localhost:8000/dash   |
---
+++ app.py
@@ -0,0 +1,34 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.wsgi import WSGIMiddleware
+
+from dashapp import create_dash_app
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_main():
+    return {
+        "routes": [
+            {"method": "GET", "path": "/", "summary": "Landing"},
+            {"method": "GET", "path": "/status", "summary": "App status"},
+            {"method": "GET", "path": "/dash", "summary": "Sub-mounted Dash application"},
+        ]
+    }
+
+
+@app.get("/status")
+def get_status():
+    return {"status": "ok"}
+
+
+# A bit odd, but the only way I've been able to get prefixing of the Dash app
+# to work is by allowing the Dash/Flask app to prefix itself, then mounting
+# it to root
+dash_app = create_dash_app(routes_pathname_prefix="/dash/")
+app.mount("/", WSGIMiddleware(dash_app.server))
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000)
